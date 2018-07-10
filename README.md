4478fee3635db85c4e08964c637524e90dc545b7
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-07-10 14:00:25 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AndroidConstraintLayoutRatio
+https://stackoverflow.com/a/51258852/5381331
---
+++ app/src/main/java/toong/com/androidconstraintlayoutratio/A.java
@@ -0,0 +1,13 @@
+package toong.com.androidconstraintlayoutratio;
+
+import android.support.annotation.NonNull;
+
+public class A implements Comparable{
+
+
+    @Override
+    public int compareTo(@NonNull Object o) {
+        return 0;
+
+    }
+}
